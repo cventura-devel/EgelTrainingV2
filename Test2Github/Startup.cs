12cce3c12c0a69e0805d6e136c566d13ef99e53a
cvent <cvent@SKYNET-GDN>
2016-07-09 15:12:28 -0600

    Add project files.

---
+++ Test2Github/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test2Github.Startup))]
+namespace Test2Github
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
